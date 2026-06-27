40403d4ead84e7fcff5f8fca2dc18c0576ae7a99
Ground-Zerro <ksa65s571dozor@gmail.com>
2026-06-27 20:25:17 +1100

    Update nuxt.config.ts

--- src/nuxt.config.ts
+++ src/nuxt.config.ts
@@ -25,125 +25,37 @@ export default defineNuxtConfig({
   },
   css: ['~/app.css'],
   i18n: {
-    // https://i18n.nuxtjs.org/docs/guide/server-side-translations
+    lazy: true,
+    langDir: 'i18n/locales/',
     experimental: {
       localeDetector: './localeDetector.ts',
     },
-    // https://wg-easy.github.io/wg-easy/latest/contributing/translation/
     locales: [
-      {
-        code: 'en',
-        language: 'en-US',
-        name: 'English',
-      },
-      {
-        code: 'de',
-        language: 'de-DE',
-        name: 'Deutsch',
-      },
-      {
-        code: 'es',
-        language: 'es-ES',
-        name: 'Español',
-      },
-      {
-        code: 'it',
-        language: 'it-IT',
-        name: 'Italiano',
-      },
-      {
-        code: 'fr',
-        language: 'fr-FR',
-        name: 'Français',
-      },
-      {
-        code: 'ko',
-        language: 'ko-KR',
-        name: '한국어',
-      },
-      {
-        code: 'ru',
-        language: 'ru-RU',
-        name: 'Русский',
-      },
-      {
-        code: 'uk',
-        language: 'uk-UA',
-        name: 'Українська',
-      },
-      {
-        code: 'zh-CN',
-        language: 'zh-CN',
-        name: '简体中文',
-      },
-      {
-        code: 'zh-HK',
-        language: 'zh-HK',
-        name: '繁體中文（香港）',
-      },
-      {
-        code: 'zh-TW',
-        language: 'zh-TW',
-        name: '正體中文 (台灣)',
-      },
-      {
-        code: 'pl',
-        language: 'pl-PL',
-        name: 'Polski',
-      },
-      {
-        code: 'cs',
-        language: 'cs-CZ',
-        name: 'Čeština',
-      },
-      {
-        code: 'pt-BR',
-        language: 'pt-BR',
-        name: 'Português (Brasil)',
-      },
-      {
-        code: 'tr',
-        language: 'tr-TR',
-        name: 'Türkçe',
-      },
-      {
-        code: 'bn',
-        language: 'bn-BD',
-        name: 'বাংলা',
-      },
-      {
-        code: 'id',
-        language: 'id-ID',
-        name: 'Bahasa Indonesia',
-      },
-      {
-        code: 'nl',
-        language: 'nl-NL',
-        name: 'Nederlands',
-      },
-      {
-        code: 'nb',
-        language: 'nb-NO',
-        name: 'Norsk bokmål',
-      },
-      {
-        code: 'bg',
-        language: 'bg-BG',
-        name: 'Български',
-      },
-      {
-        code: 'gl',
-        language: 'gl-ES',
-        name: 'Galego',
-      },
-      {
-        code: 'vi',
-        language: 'vi-VN',
-        name: 'Tiếng Việt',
-      },
+      { code: 'en', language: 'en-US', name: 'English', file: 'en.json' },
+      { code: 'de', language: 'de-DE', name: 'Deutsch', file: 'de.json' },
+      { code: 'es', language: 'es-ES', name: 'Español', file: 'es.json' },
+      { code: 'it', language: 'it-IT', name: 'Italiano', file: 'it.json' },
+      { code: 'fr', language: 'fr-FR', name: 'Français', file: 'fr.json' },
+      { code: 'ko', language: 'ko-KR', name: '한국어', file: 'ko.json' },
+      { code: 'ru', language: 'ru-RU', name: 'Русский', file: 'ru.json' },
+      { code: 'uk', language: 'uk-UA', name: 'Українська', file: 'uk.json' },
+      { code: 'zh-CN', language: 'zh-CN', name: '简体中文', file: 'zh-CN.json' },
+      { code: 'zh-HK', language: 'zh-HK', name: '繁體中文（香港）', file: 'zh-HK.json' },
+      { code: 'zh-TW', language: 'zh-TW', name: '正體中文 (台灣)', file: 'zh-TW.json' },
+      { code: 'pl', language: 'pl-PL', name: 'Polski', file: 'pl.json' },
+      { code: 'cs', language: 'cs-CZ', name: 'Čeština', file: 'cs.json' },
+      { code: 'pt-BR', language: 'pt-BR', name: 'Português (Brasil)', file: 'pt-BR.json' },
+      { code: 'tr', language: 'tr-TR', name: 'Türkçe', file: 'tr.json' },
+      { code: 'bn', language: 'bn-BD', name: 'বাংলা', file: 'bn.json' },
+      { code: 'id', language: 'id-ID', name: 'Bahasa Indonesia', file: 'id.json' },
+      { code: 'nl', language: 'nl-NL', name: 'Nederlands', file: 'nl.json' },
+      { code: 'nb', language: 'nb-NO', name: 'Norsk bokmål', file: 'nb.json' },
+      { code: 'bg', language: 'bg-BG', name: 'Български', file: 'bg.json' },
+      { code: 'gl', language: 'gl-ES', name: 'Galego', file: 'gl.json' },
+      { code: 'vi', language: 'vi-VN', name: 'Tiếng Việt', file: 'vi.json' },
     ],
     defaultLocale: 'en',
-    vueI18n: './i18n.config.ts',
+    vueI18n: './i18n/i18n.config.ts',
     strategy: 'no_prefix',
     detectBrowserLanguage: {
       useCookie: true,
@@ -166,16 +78,10 @@ export default defineNuxtConfig({
             if (id.includes('@vueuse')) {
               return 'vendor-vueuse';
             }
-            if (id.includes('@intlify/') || id.includes('/vue-i18n/')) {
+            if (id.includes('intlify') || id.includes('vue-i18n')) {
               return 'vendor-i18n';
             }
-            if (
-              id.includes('/node_modules/@vue/') ||
-              id.includes('/node_modules/vue/')
-            ) {
-              return 'vendor-vue';
-            }
-            if (id.includes('/node_modules/pinia/')) {
+            if (id.includes('/node_modules/pinia/') || id.includes('node_modules/pinia/')) {
               return 'vendor-pinia';
             }
           },
